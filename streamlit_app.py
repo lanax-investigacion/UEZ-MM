ff36fa513b02b7f8eced0ac7f48f5cab497efc45
lanax-investigacion <122426710+lanax-investigacion@users.noreply.github.com>
2023-01-10 23:32:34 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
-import streamlit as st
-import pandas as pd
-import pydeck as pdk
-
-print("Hello World")
+st.title ("this is the app title")
+st.header("this is the markdown")
+st.markdown("this is the header")
+st.subheader("this is the subheader")
+st.caption("this is the caption")
+st.code("x=2021")
+st.latex(r''' a+a r^1+a r^2+a r^3 ''')
