c619d3597bf93a65470265767041e026836d851e
lanax-investigacion <122426710+lanax-investigacion@users.noreply.github.com>
2023-01-11 00:02:00 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,16 @@
 #Importaciones
 import streamlit as st
-import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
 
 #Texto
 st.title ("Unidades Economicas de la Zona Metropolitana de Monterrey [Noviembre/2022]")
 st.markdown("El objetivo es analizar las UE de la ZMM en un mapa")
 
-#Histograma
-rand=np.random.normal(1, 2, size=20)
-fig, ax = plt.subplots()
-ax.hist(rand, bins=15)
-st.pyplot(fig)
+#Grafica
+import pandas as pd
+import numpy as np
+df= pd.DataFrame(
+    np.random.randn(10, 2),
+    columns=['x', 'y'])
+st.line_chart(df
