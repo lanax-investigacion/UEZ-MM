7b382cf6f4fc0cc3527aa49669d5548e20fe13c3
lanax-investigacion <122426710+lanax-investigacion@users.noreply.github.com>
2023-01-11 00:16:04 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,12 @@
+#Importaciones
 import streamlit as st
 import pandas as pd
-import folium
+import numpy as np
 
-# Load the data
-data = pd.read_csv("mexico_data.csv")
+#Texto
+st.title ("Unidades Economicas de la Zona Metropolitana de Monterrey [Noviembre/2022]")
+st.markdown("El objetivo es analizar las UE de la ZMM en un mapa")
 
-# Create a map
-m = folium.Map(location=[data['lat'].mean(), data['lon'].mean()], zoom_start=6)
-
-# Add markers to the map
-for _, row in data.iterrows():
-    folium.CircleMarker(location=[row['lat'], row['lon']], radius=5, color='red', fill=True, fill_color='red').add_to(m)
-
-# Show the map
-st.write(m)
-
-# Add a select box to filter the data
-selected_column = st.selectbox("Select a column to filter by", data.columns)
-value = st.text_input("Enter a filter value")
-
-# Show only the data that matches the filter
-filtered_data = data[data[selected_column] == value]
-st.dataframe(filtered_data)
+#Mapa
+df = pd.DataFrame([25.72939591, -100.3622204],columns=['lat', 'lon'])
+st.map(df)
