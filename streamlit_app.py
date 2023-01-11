cf64a9ebf48f85ab687417564f738c809b84e0d9
lanax-investigacion <122426710+lanax-investigacion@users.noreply.github.com>
2023-01-11 00:36:38 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 # Allow the user to upload the file
-file = st.file_uploader("UE_ZMM_LAT_LON_11_2022.csv", type="csv")
+file = st.file_uploader("UE_ZMM_LAT_LON_11_2022.csv")
 
 #Read the CSV
 data = pd.read_csv(file)
