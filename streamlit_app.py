acc26974242c03522466f82f58f9116431b18d1c
lanax-investigacion <122426710+lanax-investigacion@users.noreply.github.com>
2023-01-11 00:45:52 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+
+#Texto
+st.title ("Unidades Economicas de la Zona Metropolitana de Monterrey [Noviembre/2022]")
+st.markdown("El objetivo es analizar las UE de la ZMM en un mapa")
 
 #Read the CSV
 data = pd.read_csv("UE_ZMM_LAT_LON_11_2022.csv")
 
-#Display the dataframe
-st.dataframe(data)
