4d39f7b037fef864671c0dcd44ebd2fcd9ad6403
lanax-investigacion <122426710+lanax-investigacion@users.noreply.github.com>
2023-01-11 00:05:05 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,4 @@ import numpy as np
 st.title ("Unidades Economicas de la Zona Metropolitana de Monterrey [Noviembre/2022]")
 st.markdown("El objetivo es analizar las UE de la ZMM en un mapa")
 
-#Grafica
-df= pd.DataFrame(
-    np.random.randn(10, 2),
-    columns=['x', 'y'])
-st.line_chart(df
+
